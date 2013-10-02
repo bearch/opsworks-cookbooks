e45256194df3c08f73e002cbc49b5193cfccc78c
Munjal.David.Aaron <agile_check_in@Munjals-MacBook-Pro-7.local>
2013-10-02 18:43:41 -0500

    [#00] Salt n' peppa's here

--- orby/recipes/install_bcrypt.rb
+++ orby/recipes/install_bcrypt.rb
@@ -1,12 +1,10 @@
-puts '-----HDKAWJHDKWJADHAWD &&&&&&&&&&&&&&&&&&&&&&&&&&&&-----------'
-extract_path = "/home/deploy/.bundler/core/ruby/2.0.0/gems/bcrypt-ruby-3.1.1/ext/mri/"
+munjal_path = "/home/deploy/.bundler/core/ruby/2.0.0/gems/bcrypt-ruby-3.1.1/ext/mri/"
 bash "install_bcrypt" do
   user "root"
-  cwd extract_path
-  puts '-----HDKAWJHDKWJADHAWD %%%%%%%%%%%%%%%%%%%%%%%%%%-----------'
+  cwd munjal_path
   code <<-EOC
     ruby extconf.rb
     make
     make install
-EOC
-end if File.exists?(extract_path)
+  EOC
+end if File.exists?(munjal_path)
