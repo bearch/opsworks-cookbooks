41e17a3457c0a8178f2d09f2cc238a5ae891f5a8
Munjal <agile_check_in@Munjals-MacBook-Pro-7.local>
2013-10-03 19:29:20 -0500

    [#312]Changed template values

--- orby/templates/default/restart_sidekiq_values.rb
+++ orby/templates/default/restart_sidekiq_values.rb
@@ -1,9 +1,8 @@
 #!/bin/bash
-<<RUBY_EOF
+
 cd <%= @sidekiq_config_values[:current_path] %>
 
-
-<% @pid_file_default = @sidekiq_config_values[:pids_path]%>/sidekiq_default.pid %>
+<% @pid_file_default = "#{@sidekiq_config_values[:pids_path]}/sidekiq_default.pid" %>
 
 if [ -f <%=@pid_file_default%> ]
 then
@@ -12,11 +11,10 @@ rm -rf <%=@pid_file_default%>
 fi
 
 bundle exec sidekiq \
-        -e <%= @sidekiq_config_values[:rack_env] %> \
-        -P <%= @sidekiq_config_values[:pid_file_default] %> \
-        -C <%= @sidekiq_config_values[:current_path] %>/config/sidekiq.yml \
-        -L <%= @sidekiq_config_values[:log_path] %>/sidekiq.log  \
+        -e <%=@sidekiq_config_values[:rack_env] %> \
+        -P <%=@pid_file_default%> \
+        -C <%=@sidekiq_config_values[:current_path]%>/config/sidekiq.yml \
+        -L <%=@sidekiq_config_values[:log_path]%>/sidekiq.log  \
         -d \
         -q default,notifier,importer
 
-RUBY_EOF
