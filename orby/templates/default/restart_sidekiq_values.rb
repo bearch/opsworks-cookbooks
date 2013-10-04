44f11f239f0de977285dadc64bd9d456998c6f2d
Munjal <agile_check_in@Munjals-MacBook-Pro-7.local>
2013-10-03 21:14:20 -0500

    [#312]Adding enterprise and appstore queue

--- orby/templates/default/restart_sidekiq_values.rb
+++ orby/templates/default/restart_sidekiq_values.rb
@@ -18,12 +18,27 @@ function start_sidekiq_default_queue {
     -q default,notifier,importer
 }
 
+function start_sidekiq_special_queue {
+  bundle exec sidekiq \
+  -e $1 \
+  -P $2 \
+  -L $3  \
+  -c 1 \
+  -d \
+  -q $4
+}
+
+
 cd <%= @sidekiq_config_values[:current_path] %>
 
 #Sidekiq Default
 <% @pid_file_default = "#{@sidekiq_config_values[:pids_path]}/sidekiq_default.pid" %>
+<% @pid_file_enterprise = "#{@sidekiq_config_values[:pids_path]}/sidekiq_enterprise.pid" %>
+<% @pid_file_appstore = "#{@sidekiq_config_values[:pids_path]}/sidekiq_appstore.pid" %>
 
 stop_sidekiq_for <%=@pid_file_default %>
+stop_sidekiq_for <%=@pid_file_enterprise %>
+stop_sidekiq_for <%=@pid_file_appstore %>
 
 sleep 10
 
@@ -32,3 +47,12 @@ start_sidekiq_default_queue <%=@sidekiq_config_values[:rack_env] %> \
                                   <%=@sidekiq_config_values[:current_path]%>/config/sidekiq.yml \
                                   <%=@sidekiq_config_values[:log_path]%>/sidekiq.log
 
+start_sidekiq_special_queue <%=@sidekiq_config_values[:rack_env] %> \
+                                  <%=@pid_file_enterprise%> \
+                                  <%=@sidekiq_config_values[:log_path]%>/sidekiq_enterprise.log \
+                                  enterprise_notifications
+
+start_sidekiq_special_queue <%=@sidekiq_config_values[:rack_env] %> \
+                                  <%=@pid_file_appstore%> \
+                                  <%=@sidekiq_config_values[:log_path]%>/sidekiq_appstore.log \
+                                  app_store_notifications
