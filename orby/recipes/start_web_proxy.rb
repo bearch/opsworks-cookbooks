4cef134126ef933673b58fd40019680ec208153b
Aaron Lee <wwkeyboard@gmail.com>
2013-10-18 14:22:25 -0500

    [359] sudo all the things

--- orby/recipes/start_web_proxy.rb
+++ orby/recipes/start_web_proxy.rb
@@ -1,8 +1,9 @@
 bash "start_web_proxy" do
   code <<-EOC
-    nginx -s stop
+    cd /srv/www/mobile_web/current
+    sudo nginx -s stop
     sudo bundle install
-    rake minify
-    nginx -c /srv/www/mobile_web/current/config/nginx.staging.conf -p /srv/www/mobile_web/current/
+    sudo rake minify
+    sudo nginx -c /srv/www/mobile_web/current/config/nginx.staging.conf -p /srv/www/mobile_web/current/
 EOC
 end
