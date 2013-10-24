4245f03fc586540e7996cf1220ad6a4cd1f57db8
Aaron Lee <wwkeyboard@gmail.com>
2013-10-24 12:02:14 -0500

    Adding a better nginx restart

--- orby/recipes/startwebproxy.rb
+++ orby/recipes/startwebproxy.rb
@@ -1,8 +1,4 @@
 node[:deploy].each do |application, deploy|
-  service "nginx" do
-    supports :restart => true, :reload => true
-  end
-
   template "nginx.conf" do
     path "#{node[:nginx][:dir]}/nginx.conf"
     source "nginx.conf.erb"
@@ -12,5 +8,6 @@ node[:deploy].each do |application, deploy|
     owner "root"
     group "root"
     mode 0644
+    notifies :reload, "service[nginx]", :delayed
   end
 end
