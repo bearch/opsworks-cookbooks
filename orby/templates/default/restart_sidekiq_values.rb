5f258ea8fef80f997271f3a2271f54d46fdea2f7
Aaron Lee <wwkeyboard@gmail.com>
2013-11-07 10:59:32 -0600

    Adding more sidekiq workers

--- orby/templates/default/restart_sidekiq_values.rb
+++ orby/templates/default/restart_sidekiq_values.rb
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 function stop_sidekiq_for {
+    stop_sidekiq "#{@sidekiq_config_values[:pids_path]}/sidekiq_$1.pid"
+}
+
+function stop_sidekiq {
   if [ -f $1 ]
   then
   kill -TERM `cat $1` || true
@@ -15,44 +19,38 @@ function start_sidekiq_default_queue {
     -C $3 \
     -L $4  \
     -d \
-    -q default,notifier,importer
+    -q notifier,importer,default
 }
 
 function start_sidekiq_special_queue {
   bundle exec sidekiq \
-  -e $1 \
-  -P $2 \
-  -L $3  \
+      -e <%=@sidekiq_config_values[:rack_env] %> \
+      -P "#{@sidekiq_config_values[:pids_path]}/sidekiq_$1" \
+      -L "#{@sidekiq_config_values[:log_path]}/sidekiq_$1"  \
   -c 1 \
   -d \
-  -q $4
+  -q $1
 }
 
 
 cd <%= @sidekiq_config_values[:current_path] %>
 
-#Sidekiq Default
-<% @pid_file_default = "#{@sidekiq_config_values[:pids_path]}/sidekiq_default.pid" %>
-<% @pid_file_enterprise = "#{@sidekiq_config_values[:pids_path]}/sidekiq_enterprise.pid" %>
-<% @pid_file_appstore = "#{@sidekiq_config_values[:pids_path]}/sidekiq_appstore.pid" %>
-
-stop_sidekiq_for <%=@pid_file_default %>
-stop_sidekiq_for <%=@pid_file_enterprise %>
-stop_sidekiq_for <%=@pid_file_appstore %>
+stop_sidekiq_for default
+stop_sidekiq_for enterprise_notifications
+stop_sidekiq_for app_store_notifications
+stop_sidekiq_for apns_worker
+stop_sidekiq_for foursquare_importer
+stop_sidekiq_for yelp_importer
 
 sleep 10
 
 start_sidekiq_default_queue <%=@sidekiq_config_values[:rack_env] %> \
-                                  <%=@pid_file_default%> \
-                                  <%=@sidekiq_config_values[:current_path]%>/config/sidekiq.yml \
-                                  <%=@sidekiq_config_values[:log_path]%>/sidekiq.log
+                            <%=@pid_file_default%> \
+                            <%=@sidekiq_config_values[:current_path]%>/config/sidekiq.yml \
+                            <%=@sidekiq_config_values[:log_path]%>/sidekiq.log
 
-start_sidekiq_special_queue <%=@sidekiq_config_values[:rack_env] %> \
-                                  <%=@pid_file_enterprise%> \
-                                  <%=@sidekiq_config_values[:log_path]%>/sidekiq_enterprise.log \
-                                  enterprise_notifications
-
-start_sidekiq_special_queue <%=@sidekiq_config_values[:rack_env] %> \
-                                  <%=@pid_file_appstore%> \
-                                  <%=@sidekiq_config_values[:log_path]%>/sidekiq_appstore.log \
-                                  app_store_notifications
+start_sidekiq_special_queue enterprise_notifications
+start_sidekiq_special_queue app_store_notifications
+start_sidekiq_special_queue apns_worker
+start_sidekiq_special_queue foursquare_importer
+start_sidekiq_special_queue yelp_importer
